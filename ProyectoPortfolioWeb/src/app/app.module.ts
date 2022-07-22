66399285b28369146bf4c4ce91443ef1fe5c55d3
SVI73 <eliasbussanich@gmail.com>
2022-07-22 17:29:35 -0300

    Implementación de Bootstrap y creación del componente acerca-de-mi

--- ProyectoPortfolioWeb/src/app/app.module.ts
+++ ProyectoPortfolioWeb/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeMiComponent } from './components/acerca-de-mi/acerca-de-mi.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { BannerComponent } from './components/banner/banner.component';
     LogoArgProgComponent,
     RedesComponent,
     BannerComponent,
+    AcercaDeMiComponent,
   ],
   imports: [
     BrowserModule,
